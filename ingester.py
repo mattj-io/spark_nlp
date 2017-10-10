59bf0d12d8ff8e143611a45e3f59faf98c16ea07
Matt Jarvis <mjarvis@mesosphere.com>
2017-10-10 17:04:33 +0100

    Ignore SSL cert

--- ingester.py
+++ ingester.py
@@ -5,6 +5,7 @@ Load data for demo
 
 import urllib2
 import json
+import ssl
 from kafka import KafkaProducer
 
 def main():
@@ -15,10 +16,13 @@ def main():
 
     producer = KafkaProducer(value_serializer=lambda m: json.dumps(m).encode('utf-8'),
                              bootstrap_servers='localhost:9092')
-
+    
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
     data_url = 'https://raw.githubusercontent.com/mattj-io/spark_nlp/master/stack.json'
 
-    data_file = urllib2.urlopen(data_url)
+    data_file = urllib2.urlopen(data_url, context=ctx)
 
     for line in data_file:
         producer.send(kafka_topic, json.loads(line))
